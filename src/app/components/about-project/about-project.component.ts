c84655cbbd2dae7ce579e32a3f8f4e99e96bf119
AddwinFernando <addwinfernando.jacob@kumaran.com>
2023-10-19 12:52:06 +0530

    hjjh

--- src/app/components/about-project/about-project.component.ts
+++ src/app/components/about-project/about-project.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'about-project',
   templateUrl: './about-project.component.html',
   styleUrls: ['./about-project.component.css']
 })
-export class AboutProjectComponent {
+export class AboutProjectComponent implements OnInit {
+  constructor(private prod:ProductService){}
+  ngOnInit(): void {
+    if (this.prod.getcachedproducts()) {
+      this.prod.getProducts().subscribe({
+        next: (data: Product[]) => {
+          this.prod.setcacheproducts(data as Product[]);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    }
+  }
 
 }
